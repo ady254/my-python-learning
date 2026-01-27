c3d95946b1465c3f8f72cb4d12b5fb39778c04ca
ady254 <itsadnanahmad5@gmail.com>
2026-01-27 23:57:31 +0530

    Fix statement file

---
+++ 01_basics/statement.py
@@ -0,0 +1,49 @@
+"""
+Topic: Python Statement
+Date: 2026-01-26
+Concepts Covered:
+- Python Statement
+
+"""
+#A computer program is a list of "instructions" to be "executed" by a computer.
+
+#In a programming language, these programming instructions are called statements.
+
+#The following statement prints the text "Python is cool!" to the screen:
+
+print("Python is cool!")
+
+#The print() function is a built-in function in Python that is used to output data to the console or terminal.
+
+#In Python, a line of code ends automatically at the end of the line. 
+# You don’t need to use a semicolon (;) like in languages such as Java or C.
+
+#Many Statements
+
+#The statements are executed one by one, in the same order as they are written:
+
+print("Hello")
+print("World")
+print("Cool!")
+print("Python is fun!")
+
+#The first statement is executed first (print "Hello World!").
+#Then, the second statement is executed (print "Python is cool!").
+#Then, the third statement is executed (print "Python is fun!").
+#And finally, the fourth statement is executed (print "Python is fun!").
+
+#Semicolons (Optional, Rarely Used)
+#Semicolons are not required in Python. You can use them to write more than one statement on the same line, but this is rarely done because it makes the code harder to read.
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+#However, if you put two statements on the same line without a separator (newline or ;), Python will give an error:
+
+print("Python is fun!") print("Really!")
+
+#The error message will be:
+#SyntaxError: invalid syntax
+
+
+
+
