ebb04c843bfbb2f5bd2cbaf65e4b2a15d4c79373
ady254 <itsadnanahmad5@gmail.com>
2026-01-29 00:03:25 +0530

    Adding print in numbers in 03_outin.py

--- 01_basics/03_outin.py
+++ 01_basics/03_outin.py
@@ -30,3 +30,31 @@ Hello World! I will print on the same line.
 
 #NOTE:Note that we add a space after end=" " for better readability.
  
+
+
+
+
+ """
+ Print Numbers
+
+
+"""""
+# You can  print many numbers using print() function
+# However, unlike text, we don't put numbers inside double quotes:
+
+print(2)
+print(2006)
+print(50000)
+
+#You can also do math inside the print() function:
+
+print(2 + 2)
+print(2006 - 1)
+print(50000 * 2)
+print(50000 / 2)
+
+#output:
+4
+2005
+100000
+25000.0
