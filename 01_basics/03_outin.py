f254e0bbc27b9a13baaae5fcb76f7cdffbffe75f
ady254 <itsadnanahmad5@gmail.com>
2026-01-28 23:56:48 +0530

    Forgot to Save file

---
+++ 01_basics/03_outin.py
@@ -0,0 +1,32 @@
+"""
+Topic: Python Output/Input
+Date: 2026-01-26
+Concepts Covered:
+- Python Text
+- Python Numbers
+
+"""
+
+#Text in Python must be inside quotes. You can use either " double quotes or ' single quotes:
+
+print("Hello, World!")
+print('Hello, World!')
+
+#Python will give an error:
+print(Hello, World!)
+# this will cause an error
+
+#Print Without a New Line
+
+#If you want to print multiple words on the same line, you can use the end parameter:
+print("Hello", end=" ")
+print("World!")
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+#output:
+Hello World! I will print on the same line.
+
+#NOTE:Note that we add a space after end=" " for better readability.
+ 
