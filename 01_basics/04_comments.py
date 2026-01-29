d4e0649e71696f33ea9bf78e9e0b0833315f6e7f
ady254 <itsadnanahmad5@gmail.com>
2026-01-29 15:08:21 +0530

    Topic Comment

---
+++ 01_basics/04_comments.py
@@ -0,0 +1,77 @@
+"""
+Topic: Python Comments
+Date: 2026-01-28
+Concepts Covered:
+- Creating a  Comments
+- Multi-line Comments
+- Why we use Comments
+
+"""
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code.
+# Comments Start with #
+
+#Example 
+
+print("Hello Python") #This is a comment
+
+#print("Hello Python") 
+print("Hi I'm Python")
+
+# A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code:
+
+
+# Multi-line Comments
+   #Python does not really have a syntax for multiline comments.
+
+   #To add a multiline comment you could insert a # for each line:
+
+   # Example:
+
+   #This is a comment
+   #written in
+   #more than just one line
+print("Hello, World!")
+
+
+   
+# Since Python ignores string literals that are not assigned to any variable, you can use triple-quoted strings to write multi-line comments in your program.
+
+   # Example:
+   """
+   Topic: Python Comments
+   Date: 2026-01-28
+   Concepts Covered:
+
+   """
+
+   print("Hello World")
+
+#Questions:
+    # Which Character is used to start a comment?
+      
+       #Answer: #
+
+    # Can we use multi-line comments in Python?
+      
+       #Answer: Yes
+
+    # Why we use comments?
+      
+       #Answer: To explain the code
+
+    # Use a multiline string to make the a multiline comment:
+    
+    # Answer:
+    """
+    This is a comment
+    written in 
+    more than just one line
+
+    """
+
+
+
+
+
