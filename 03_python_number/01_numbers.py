df0b16563917eb1d8963303ec963f74f24100240
ady254 <itsadnanahmad5@gmail.com>
2026-01-30 23:55:07 +0530

    Numbers in Python

---
+++ 03_python_number/01_numbers.py
@@ -0,0 +1,63 @@
+"""
+topic: Python Numbers
+Date: 2026-01-30
+Concepts Covered:
+- Numbers
+- Integers
+- Floats
+- Complex Numbers
+"""
+
+# Integers
+# Integers are whole numbers, positive or negative, without decimals, of unlimited length.
+
+# Example:
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Output:
+# <class 'int'>
+# <class 'int'>
+# <class 'int'>
+
+# Floats
+# Floats are real numbers, positive or negative, containing one or more decimals.
+
+# Example:
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Output:
+# <class 'float'>
+# <class 'float'>
+# <class 'float'>
+
+# Complex Numbers
+# Complex numbers are real numbers, positive or negative, containing one or more decimals.
+
+# Example:
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Output:
+# <class 'complex'>
+# <class 'complex'>
+# <class 'complex'>"""
