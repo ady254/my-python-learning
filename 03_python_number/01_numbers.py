d83066539f21b6122fa620e187cb8f25e86fef7e
ady254 <itsadnanahmad5@gmail.com>
2026-01-31 00:05:53 +0530

    added type conversion and random() function

--- 03_python_number/01_numbers.py
+++ 03_python_number/01_numbers.py
@@ -6,6 +6,7 @@ Concepts Covered:
 - Integers
 - Floats
 - Complex Numbers
+- Type Conversion
 """
 
 # Integers
@@ -61,3 +62,50 @@ print(type(z))
 # <class 'complex'>
 # <class 'complex'>
 # <class 'complex'>"""
+
+# Type Conversion
+# Type conversion is the process of converting one data type to another.
+
+# Example:
+
+#convert from int to float:
+x = float(1)
+
+#convert from float to int:
+y = int(2.8)
+
+#convert from int to complex:
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Output:
+# 1.0
+# 2
+# (1+0j)
+# <class 'float'>
+# <class 'int'>
+# <class 'complex'>
+
+# NOTE: You cannot convert complex numbers into another number type.
+
+# Random(): Python does not have a random() function to make a random number
+# But Python has a built-in module called random that can be used to make random numbers
+
+# Example: Import the random module, and display a random number from 1 to 9:
+
+import random
+
+print(random.randrange(1, 10))
+
+# Output: 2
+
+
+
