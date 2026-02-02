450fba80576a845f51ec19ff8ed2a0808680cdd4
ady254 <itsadnanahmad5@gmail.com>
2026-02-02 23:45:24 +0530

    casting

---
+++ 04_casting/01_casting.py
@@ -0,0 +1,32 @@
+"""
+topic: Python Casting
+Date: 2026-02-03
+Concepts Covered:
+- Casting
+"""
+
+# Casting
+# Casting is the process of converting one data type to another.
+
+# Example:
+
+x = int(1)
+y = float(2)
+z = complex(3)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Output:
+# 1
+# 2.0
+# (3+0j)
+# <class 'int'>
+# <class 'float'>
+# <class 'complex'>
+
