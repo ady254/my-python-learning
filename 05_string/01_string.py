f2369e8fb19fa0b5dc11fb2644b8f7e4f9d3cf87
ady254 <itsadnanahmad5@gmail.com>
2026-02-02 23:50:41 +0530

    String

---
+++ 05_string/01_string.py
@@ -0,0 +1,33 @@
+"""
+Topic: String
+Date: 2026-02-03
+Concepts Covered:
+- String
+"""
+
+# What is String?
+# Ans: String is a sequence of characters.
+
+# How to create a string?
+# Ans: String can be created using single quotes, double quotes, or triple quotes.
+
+# Example:
+
+x = "Hello World"
+y = 'Hello World'
+z = """Hello World"""
+
+print(x)
+print(y)
+print(z)
+
+# Output:
+# Hello World
+# Hello World
+# Hello World
+
+
+
+
+
+
