a6f4ed45004eb42f876ffa22f6b8a6cd43e35d24
ady254 <itsadnanahmad5@gmail.com>
2026-02-03 23:57:19 +0530

    Modify Strings

---
+++ 05_string/03_modifyString.py
@@ -0,0 +1,71 @@
+"""
+Topic: Modify String
+Date: 2026-02-03
+Concept Covered:
+-upper case
+-lower case
+-strip
+-replace
+-split
+-capitalize
+
+"""
+# Upper case: The upper() method returns the string in upper case.
+# Example 1:
+
+x = "Hello Adnan"
+
+print(x.upper())
+
+# Output:
+# HELLO ADNAN
+
+# Lower case: The lower() method returns the string in lower case.
+# Example 2:
+
+x = "Hello Adnan"
+
+print(x.lower())
+
+# Output:
+# hello adnan
+
+# Strip: The strip() method removes whitespace from the beginning and end of a string.
+# Example 3:
+
+x = "   Hello Adnan   "
+
+print(x.strip())
+
+# Output:
+# Hello Adnan
+
+# Replace: The replace() method replaces a specified value with another value in a string.
+# Example 4:
+
+x = "Hello Adnan"
+
+print(x.replace("Adnan", "Ahmad"))
+
+# Output:
+# Hello Ahmad
+
+# Split: The split() method splits a string into a list.
+# Example 5:
+
+x = "Hello Adnan"
+
+print(x.split())
+
+# Output:
+# ['Hello', 'Adnan']
+
+# Capitalize: The capitalize() method returns the string in capitalize case.
+# Example 6:
+
+x = "Hello Adnan"
+
+print(x.capitalize())
+
+# Output:
+# Hello adnan
