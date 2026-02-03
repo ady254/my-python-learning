ac45a4250f52ae46f3c5cf81dba222c75aea72e4
ady254 <itsadnanahmad5@gmail.com>
2026-02-03 23:59:56 +0530

    concatenation

---
+++ 05_string/04_concatenation.py
@@ -0,0 +1,16 @@
+"""
+Topic: Concatenation
+Date: 2026-02-04
+Concept Covered:
+- Concatenation
+"""
+# Concatenation: The + operator is used to concatenate two strings.
+# Example 1:
+
+x = "Hello"
+y = "Adnan"
+
+print(x + y)
+
+# Output:
+# HelloAdnan
