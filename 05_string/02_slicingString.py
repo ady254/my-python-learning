b899160655550ce8bda7aff53d956a18594a1e32
ady254 <itsadnanahmad5@gmail.com>
2026-02-03 23:40:25 +0530

    Slicing

---
+++ 05_string/02_slicingString.py
@@ -0,0 +1,22 @@
+"""
+Topic: Slicing String
+Date: 2026-02-04
+Concepts Covered:
+- Slicing String
+"""
+
+# What is Slicing String?
+# Ans: Slicing String is a way to extract a portion of a string.
+
+# How to slice a string?
+# Ans: String can be sliced using slicing operator.
+#NOTE: The first character is at index 0.
+
+# Example:
+
+x = "Hello, World!"
+
+print(x[:5])
+
+
+
