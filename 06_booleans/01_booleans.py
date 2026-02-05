aa0d2efbe23b785f8d98a4a9752cc43a36753051
ady254 <itsadnanahmad5@gmail.com>
2026-02-05 23:04:33 +0530

    boolean

---
+++ 06_booleans/01_booleans.py
@@ -0,0 +1,31 @@
+"""
+Topic: Booleans
+Date: 2026-02-05
+Concept covered: 
+-Booleans
+
+"""
+# What is boolean?
+# Boolean is a data type that can have only two values: True or False
+
+# Example:
+print(10 > 5)
+print(10 == 9)
+print(10 < 9)
+
+# Output:
+# True
+# False
+# False
+
+# Print a message based on whether the condition is True or False
+a = 200 
+b = 33
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a") 
+
+# Output:
+# b is not greater than a
