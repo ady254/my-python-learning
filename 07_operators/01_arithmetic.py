88164ae259e3862397e234993056299c08ba9f32
ady254 <itsadnanahmad5@gmail.com>
2026-02-06 17:44:07 +0530

    arithmetic operators

---
+++ 07_operators/01_arithmetic.py
@@ -0,0 +1,57 @@
+"""
+Topic: Arithmetic Operators
+Concept Covered: 
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Modulus
+- Exponentiation
+- Floor Division
+"""
+
+# Exampple: 
+
+x = 10
+y = 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+# Output: 
+# 13
+# 7
+# 30
+# 3.3333333333333335
+# 1
+# 1000
+# 3
+
+# Note: 
+# Divison in python
+# 1. Always returns a float 
+# 2. // returns the floor value: means it rounds down to the nearest integer
+
+# Example:
+# Division returns a float
+x = 12
+y = 5
+print(x / y)
+
+# Output: 
+# 2.4
+
+# Example: Floor Division
+x = 12
+y = 5
+print(x // y)
+
+# Output: 
+# 2
+
+
