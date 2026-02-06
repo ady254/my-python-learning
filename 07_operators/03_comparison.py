302c9cf8634233fc6d02dfa2c299db60b8605181
ady254 <itsadnanahmad5@gmail.com>
2026-02-06 17:59:27 +0530

    comparison operator

---
+++ 07_operators/03_comparison.py
@@ -0,0 +1,26 @@
+"""
+Topic: Comparison Operators
+Date: 2026-02-06
+Concept Covered:
+- Comparison Operators
+"""
+
+# Comparison Operators: are used to compare two values and return a boolean value (True or False).  
+
+# Types of comparison operators:
+# 1. Equal to (==)
+# 2. Not equal to (!=)
+# 3. Greater than (>)
+# 4. Less than (<)
+# 5. Greater than or equal to (>=)
+# 6. Less than or equal to (<=)
+
+# Example:
+x = 5
+y = 10
+print(x == y)  # False
+print(x != y)  # True
+print(x > y)   # False
+print(x < y)   # True
+print(x >= y)  # False
+print(x <= y)  # True
