49131497c42150ed4373da4f3d748faf1871fcf1
ady254 <itsadnanahmad5@gmail.com>
2026-02-08 22:44:36 +0530

    Logical Operator

---
+++ 07_operators/04_logical.py
@@ -0,0 +1,20 @@
+"""
+Topic: Logical Operators
+Date: 2026-02-2026
+Concept Covered:
+- Logical Operators
+"""
+
+# Logical Operators: are used to combine two or more conditions and return a boolean value (True or False).
+
+# Types of logical operators:
+# 1. AND (and)
+# 2. OR (or)
+# 3. NOT (not)
+
+# Example:
+x = 5
+y = 10
+print(x and y)  # True
+print(x or y)   # True
+print(not x)    # False
