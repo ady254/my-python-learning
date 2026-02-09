8fbe08b66b76e00ae9acd887553fa9c69cd9360d
ady254 <itsadnanahmad5@gmail.com>
2026-02-09 22:35:11 +0530

    list

---
+++ 08_lists/01_list.py
@@ -0,0 +1,21 @@
+"""
+Topic: Lists
+Date: 2026-02-09
+Concept Covered:
+- Lists
+"""
+
+# Lists: are used to store multiple values in a single variable.
+# Lists are ordered, mutable (can be changed), and can contain duplicate values.
+# Lists are defined using square brackets [].
+
+
+# Example: Create a list
+
+thislist = ["apple", "banana", "mango"]
+print(thislist)
+
+# Output:
+# ['apple', 'banana', 'mango']
+
+
