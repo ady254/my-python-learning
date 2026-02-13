d1455ab8643cd82e310b91a0bf5c5515e96444c0
ady254 <itsadnanahmad5@gmail.com>
2026-02-14 00:46:11 +0530

    List method

--- 08_lists/07_list_method.py
+++ 08_lists/07_list_method.py
@@ -55,4 +55,72 @@ print(thislist)
 # Output:
 [50, 65, 23, 82, 100]
 
+# Copy method: copy() method creates a copy of the list
 
+# Example:
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+newlist = thislist.copy()
+print(newlist)
+
+# Output:
+['orange', 'mango', 'kiwi', 'pineapple', 'banana']
+
+# Join method: join() method joins the elements of a list into a string
+
+# Example:
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+newlist = " ".join(thislist)
+print(newlist)
+
+# Output:
+orange mango kiwi pineapple banana
+
+# Use the slice Operator
+# Copy of a list by using the : (slice) operator.
+
+# Example: 
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
+
+# Output:
+['apple', 'banana', 'cherry']
+
+# Join list --- There are two ways to join lists:
+
+# 1. Using the join() method
+# 2. Using the + operator
+
+# Example: 
+list1 = ["apple", "banana", "cherry"]
+list2 = ["orange", "mango", "kiwi"]
+newlist = list1 + list2
+print(newlist)
+
+# Output:
+['apple', 'banana', 'cherry', 'orange', 'mango', 'kiwi']
+
+# Another way to join two lists is by appending all the items from list2 into list1, one by one:
+
+# example:
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+# Output:
+['a', 'b', 'c', 1, 2, 3]
+
+# Example: Using extend() method
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+
+print(list1)
+
+# Output:
+['a', 'b', 'c', 1, 2, 3]
