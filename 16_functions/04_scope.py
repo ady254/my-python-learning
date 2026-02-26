8a699b21534ec5e32e8304141121b922c705cd20
ady254 <itsadnanahmad5@gmail.com>
2026-02-26 23:42:25 +0530

    scope

---
+++ 16_functions/04_scope.py
@@ -0,0 +1,27 @@
+"""
+Topic: Python Scope
+Date: 2026-02-26
+Concept Covered: 
+- Scope
+
+"""
+
+#  What is Scope?
+# - A variable is only available from inside the region it is created. This is called scope.
+
+# What is Local Scope?
+# -A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
+
+# Example:
+
+#A variable created inside a function is available inside that function:
+
+def myfunc():
+  x = 300
+  print(x)
+
+myfunc()
+
+# Output:
+# 300
+
