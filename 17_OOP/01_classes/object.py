1b5da71cf1c5c367181c0a9660047803c5e2635b
ady254 <itsadnanahmad5@gmail.com>
2026-03-01 21:48:11 +0530

    classes/object

---
+++ 17_OOP/01_classes/object.py
@@ -0,0 +1,29 @@
+"""
+Topic: Python Classes and Object
+Date: 2026-03-1
+Concept Covered:
+- Classes
+- Object
+"""
+
+# What is python classes/objects?
+# Python is an object oriented programming language.
+# A Class is like an object constructor, or a "blueprint" for creating objects.
+
+
+# Example: Create a Class
+class MyClass:
+    x = 5
+
+print(MyClass)
+
+# Output: <class '__main__.MyClass'>
+
+# Create Object:
+class MyClass()
+     x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+# Output: 5
